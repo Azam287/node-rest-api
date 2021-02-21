c1eef07c9641b64730503865ff906a419d665e53
unknown <mohdazamnoor28@gmail.com>
2021-02-21 21:11:03 +0530

    changed in posts file for new feature

--- routes/posts.js
+++ routes/posts.js
@@ -7,7 +7,7 @@ const router = express.Router();
 });*/
 
 router.get('/posts',(res,req)=>{
-    res.send('we are on posts');
+    res.send('we are on posts tygyghuhoijoijoook');
 });
 
 router.get('/specific',(res,req)=>{
