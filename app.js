efc8e55eb02b34d721ec0b7d6cb595402aa518d7
unknown <mohdazamnoor28@gmail.com>
2021-02-21 21:06:02 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,51 @@
+const express = require('express');
+
+const app = express();
+const mongoose = require('mongoose');
+require('dotenv/config');
+
+//importing routes
+const postsRoute = require('./routes/posts');
+
+//middleware
+app.use('/posts', postsRoute);
+
+//creating database
+
+mongoose.connect(
+    process.env.DB_CONNECTION, 
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    () =>  console.log("connect to database")
+);
+
+
+/*const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://azam:22778572@cluster0.pss3z.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});*/
+
+
+/*//middleware running
+app.use('/posts',()=>{
+    console.log('middlware running');
+});*/
+
+//routes
+app.get('/', (req,res)=>{
+    res.send('we are home');
+});
+
+/*app.get('/posts', (req,res)=>{
+    res.send('we are on post');
+});*/
+
+
+//listening to server
+app.listen(3000,()=>{
+    console.log('server is live');
+});
+
---
+++ routes/posts.js
@@ -0,0 +1,17 @@
+const express = require('express');
+//const { Mongoose } = require('mongoose');
+const router = express.Router();
+
+/*router.get('/',(req,res)=>{
+    res.send('we are on home');
+});*/
+
+router.get('/posts',(res,req)=>{
+    res.send('we are on posts');
+});
+
+router.get('/specific',(res,req)=>{
+    res.send('we are on specific');
+});
+
+module.exports = router;
